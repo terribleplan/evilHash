384d8282821b21779e7b348a1467be591449eeec
terribleplan <kegan@keganmyers.com>
2013-07-24 09:14:53 -0500

    Update the actual class

--- evilHash.php
+++ evilHash.php
@@ -1,12 +1,19 @@
 <?php
 /**
  * Name: evilHash.php
- * Version: 1.0
  * Author: terriblepln
  * License: WTFPL
  */
 Class EvilHash {
-	public static function hash($input, $key="ThisShouldBeAR4n|)0mzSTRNG") {
+	public static function hash($input, $key=false) {
+		$c = "EVILHASH_KEY";
+		if ($key === false) {
+			if (defined($c)) {
+				$key = constant($c);
+			} else {
+				$key = "ThisShouldBeAR4n|)0mzSTRNG";
+			}
+		}
 		$algo = array('whirlpool', 'sha512', 'ripemd320', 'haval256,5', 'tiger192,4', 'sha1', 'md5');
 		$i = 0;
 		foreach ($algo as $h) {
