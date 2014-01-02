e55ba4a6334645a33c46c125394f02451b849561
terribleplan <kegan@keganmyers.com>
2014-01-02 10:47:47 -0600

    Update evilHash.php
    bring into line with my current coding style
    remove an unnecessary line
    better names for things
    added comments

--- evilHash.php
+++ evilHash.php
@@ -5,22 +5,34 @@
  * License: WTFPL
  */
 Class EvilHash {
-	public static function hash($input, $key=false) {
-		$c = "EVILHASH_KEY";
+	public static function hash($input, $key=false, $iterations=1) {
+		//Figure out what we will use for our HMAC key
 		if ($key === false) {
-			if (defined($c)) {
-				$key = constant($c);
+			$keyConstant = "EVILHASH_KEY";
+			if (defined($keyConstant)) {
+				$key = constant($keyConstant);
 			} else {
 				$key = "ThisShouldBeAR4n|)0mzSTRNG";
 			}
 		}
-		$algo = array('whirlpool', 'sha512', 'ripemd320', 'haval256,5', 'tiger192,4', 'sha1', 'md5');
-		$i = 0;
-		foreach ($algo as $h) {
-			$input = hash_hmac($h, $i . $input, $key . $h);
-			$i++;
+		
+		//Determine the number of times each hash will be run
+		if ($iterations < 1) {
+			$iterations = 1;
 		}
+		
+		//These are the hashes that will be run, see hash_algos() for what your PHP installation supports
+		$algo = array('whirlpool', 'sha512', 'ripemd320', 'haval256,5', 'tiger192,4', 'sha1', 'md5');
+		
+		//Calculate the hash value, note that we do add information to both the key and the input data
+		$increment = 0;
+		foreach ($algo as $hashType) {
+			$hashRuns = 0;
+			while ($hashRuns < $iterations) {
+				$input = hash_hmac($hashType, $increment++ . $hashRuns++ . $input, $key . $hashType);
+			}
+		}
+		
 		return $input;
 	}
 }
-?>
